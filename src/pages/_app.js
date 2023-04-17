426d968444f3aa1ba0f37642a1d754b913acd68f
v-Alqorith <vahapcandongel@hotmail.com>
2023-04-17 18:43:08 +0100

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 import SideMenu from "@/components/SideMenu";
 import Navigation from "@/components/core/Navigation";
 import "@/styles/globals.css";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
   return (
     <div className="bg-white w-full min-h-screen flex flex-col items-center antialiased font-source">
       <Navigation />
@@ -11,7 +16,12 @@ export default function App({ Component, pageProps }) {
         <SideMenu />
       </div>
 
-      <Component {...pageProps} />
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
     </div>
   );
 }
