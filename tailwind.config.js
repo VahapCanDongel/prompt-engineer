685596ea230b5fcb5034c0f92c2c42371641f8e8
v-Alqorith <vahapcandongel@hotmail.com>
2023-04-17 18:50:57 +0100

    complete font import

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      source: ["Source Sans Pro", "sans-serif"],
+      right: ["Righteous", "cursive"],
+    },
   },
   plugins: [require("daisyui")],
 };
