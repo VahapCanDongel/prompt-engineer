e421af46cbe24425551ff180e3322788b43c293e
VahapCanDongel <vahapcandongel@hotmail.com>
2023-04-21 12:20:17 +0100

    goign to add delete operation

--- src/components/comment/CommentCard.js
+++ src/components/comment/CommentCard.js
@@ -3,6 +3,10 @@ import { useUser } from "@supabase/auth-helpers-react"
 export default function CommentCard({ user_name, user_picture, user_comment, id, user_id }) {
     const currentSignedInUser = useUser()
 
+    const handleDeleteOperation = () => {
+
+    }
+
     return (
         <div className="w-[500px] min-h-[80px] border-[1px] border-gray-300 rounded-sm p-2 my-4">
             <div className="flex gap-2 font-semibold">
@@ -14,7 +18,7 @@ export default function CommentCard({ user_name, user_picture, user_comment, id,
                 <div></div>
             ) : (
                 <div className="relative left-[460px]">
-                    <svg width="20" height="20" className=" fill-indigo-400" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="20" height="20" className=" fill-indigo-400" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" onClick={handleDeleteOperation}>
                         <path d="M15 4h5a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h5V3a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1Zm3.8 6-.613 9.2a3 3 0 0 1-2.993 2.8H8.826a3 3 0 0 1-2.993-2.796L5.205 10H18.8ZM10 11a1 1 0 0 0-1 1v7a1 1 0 1 0 2 0v-7a1 1 0 0 0-1-1Zm4 0a1 1 0 0 0-1 1v7a1 1 0 0 0 2 0v-7a1 1 0 0 0-1-1Z"></path>
                     </svg>
                 </div>
