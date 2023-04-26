ac026ea60e0106c60b02539c2a7733b728a9878b
VahapCanDongel <vahapcandongel@hotmail.com>
2023-04-26 10:18:03 +0100

    Need to fix likes issue

--- src/components/prompt/PromptCard.js
+++ src/components/prompt/PromptCard.js
@@ -61,19 +61,19 @@ export default function PromptCard({
       className="w-[500px] min-h-[80px] p-4 rounded-md bg-gray-100 my-1 flex-shrink-0"
     >
       <div onClick={handlePromptRoute}>
-      <div className="flex gap-2">
-        <img
-          src={user_icon}
-          className="w-12 h-12 rounded-full border-indigo-400 border-[1px] p-1"
-        />
-        <div className="font-semibold text-gray-500">{user}</div>
-      </div>
-      <div className="ml-14 mb-6 mt-[-17px]">
-        <div className="font-semibold text-gray-500">{title}</div>
-        <div className="text-gray-500">{content}</div>
-      </div>
+        <div className="flex gap-2">
+          <img
+            src={user_icon}
+            className="w-12 h-12 rounded-full border-indigo-400 border-[1px] p-1"
+          />
+          <div className="font-semibold text-gray-500">{user}</div>
+        </div>
+        <div className="ml-14 mb-6 mt-[-17px]">
+          <div className="font-semibold text-gray-500">{title}</div>
+          <div className="text-gray-500">{content}</div>
+        </div>
 
-      {/* <div className="flex w-full justify-between items-center">
+        {/* <div className="flex w-full justify-between items-center">
         <div className="text-gray-500 flex gap-1 text-[14px]">
           <div>Posted </div>
 
@@ -83,21 +83,22 @@ export default function PromptCard({
       </div> */}
       </div>
       <div className="flex justify-end" onClick={handlePromptLike}>
-          <svg
-            width={26}
-            height={26}
-            viewBox="0 0 24 24"
-            className="hover:fill-indigo-400 trans-smooth fill-gray-500"
-          >
-            <path d="m11.293 5.547.707.708.707-.707a5 5 0 1 1 7.07 7.071l-7.07 7.071a1 1 0 0 1-1.414 0l-7.071-7.07a5 5 0 1 1 7.07-7.072Z" />
-          </svg>
-          <div className="text-gray-500">
-            {likes.length > 0 ? likes.length : 0}
-          </div>
-          {likes.map((like, index) => (
-            <div key={index}></div>
-          ))}
-        </div>
+        <svg
+          width={26}
+          height={26}
+          viewBox="0 0 24 24"
+          className="hover:fill-indigo-400 trans-smooth fill-gray-500"
+        >
+          <path d="m11.293 5.547.707.708.707-.707a5 5 0 1 1 7.07 7.071l-7.07 7.071a1 1 0 0 1-1.414 0l-7.071-7.07a5 5 0 1 1 7.07-7.072Z" />
+        </svg>
+        <div className="text-gray-500">
+
+        </div>{likes && likes.length > 0 ? likes.length : 0}
+
+        {likes.map((like, index) => (
+          <div key={index}></div>
+        ))}
+      </div>
     </div>
   );
 }
